f0c19a95446e75486a22443cd38e9a9dabe14017
stephanienguyen314 <32085623+stephanienguyen314@users.noreply.github.com>
2020-12-11 13:29:10 -0800

    Create ActiveCountries.js

---
+++ src/components/ActiveCountries.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+
+export class ActiveCountries extends Component {
+
+    state = { 
+        sorted: [] 
+    }
+
+    componentDidMount(){
+        axios.get('https://disease.sh/v3/covid-19/countries?sort=deaths')
+            .then(res => this.setState({sorted: res.data }))
+    }
+    render() {
+        return (
+            <div>
+                <h2 style={{marginBottom:'10px', marginLeft:'25px'}}>Active Cases by Countries</h2>
+                        <div style= {{height:'250px', overflow:'auto', marginLeft: '25px',width: '260px'}}>
+                            {this.state.sorted.map((data) => (
+                                <div style={country }>
+                                <h4 >{data.country}</h4>
+                                <h4 style={{marginLeft:'15px'}}>{parseInt(data.active).toLocaleString()}</h4>
+                                </div>
+                            ))}
+                        </div>
+            </div>
+        )
+    }
+}
+
+const country = {
+    display: 'flex',
+}
+
+export default ActiveCountries
